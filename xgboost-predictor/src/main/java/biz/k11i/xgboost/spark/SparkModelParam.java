82f90c881420dbe771dcce8f7dd73fb2947a34c1
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2017-02-25 00:38:28 +0900

    Support model format generated by xgboost4j-spark

---
+++ xgboost-predictor/src/main/java/biz/k11i/xgboost/spark/SparkModelParam.java
@@ -0,0 +1,40 @@
+package biz.k11i.xgboost.spark;
+
+import biz.k11i.xgboost.util.ModelReader;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class SparkModelParam implements Serializable {
+    public static final String MODEL_TYPE_CLS = "_cls_";
+    public static final String MODEL_TYPE_REG = "_reg_";
+
+    final String modelType;
+    final String featureCol;
+    final String labelCol;
+    final String predictionCol;
+
+    // classification model only
+    final String rawPredictionCol;
+    final double[] thresholds;
+
+    public SparkModelParam(String modelType, String featureCol, ModelReader reader) throws IOException {
+        this.modelType = modelType;
+        this.featureCol = featureCol;
+        this.labelCol = reader.readUTF();
+        this.predictionCol = reader.readUTF();
+
+        if (MODEL_TYPE_CLS.equals(modelType)) {
+            this.rawPredictionCol = reader.readUTF();
+            int thresholdLength = reader.readIntBE();
+            this.thresholds = thresholdLength > 0 ? reader.readDoubleArrayBE(thresholdLength) : null;
+
+        } else if (MODEL_TYPE_REG.equals(modelType)) {
+            this.rawPredictionCol = null;
+            this.thresholds = null;
+
+        } else {
+            throw new UnsupportedOperationException("Unknown modelType: " + modelType);
+        }
+    }
+}
--- xgboost-predictor/src/main/java/biz/k11i/xgboost/util/ModelReader.java
+++ xgboost-predictor/src/main/java/biz/k11i/xgboost/util/ModelReader.java
@@ -5,6 +5,7 @@ import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UTFDataFormatException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.Charset;
@@ -42,6 +43,10 @@ public class ModelReader implements Closeable {
         return numBytesRead;
     }
 
+    public int readByteAsInt() throws IOException {
+        return stream.read();
+    }
+
     public byte[] readByteArray(int numBytes) throws IOException {
         int numBytesRead = fillBuffer(numBytes);
         if (numBytesRead < numBytes) {
@@ -57,12 +62,20 @@ public class ModelReader implements Closeable {
     }
 
     public int readInt() throws IOException {
+        return readInt(ByteOrder.LITTLE_ENDIAN);
+    }
+
+    public int readIntBE() throws IOException {
+        return readInt(ByteOrder.BIG_ENDIAN);
+    }
+
+    private int readInt(ByteOrder byteOrder) throws IOException {
         int numBytesRead = fillBuffer(4);
         if (numBytesRead < 4) {
             throw new EOFException("Cannot read int value (shortage): " + numBytesRead);
         }
 
-        return ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN).getInt();
+        return ByteBuffer.wrap(buffer).order(byteOrder).getInt();
     }
 
     public int[] readIntArray(int numValues) throws IOException {
@@ -105,6 +118,15 @@ public class ModelReader implements Closeable {
         return ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).getFloat();
     }
 
+    public int asUnsignedInt(byte[] bytes) throws IOException {
+        int result = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).getInt();
+        if (result < 0) {
+            throw new IOException("Cannot treat as unsigned int (overflow): " + result);
+        }
+
+        return result;
+    }
+
     public float readFloat() throws IOException {
         int numBytesRead = fillBuffer(4);
         if (numBytesRead < 4) {
@@ -127,6 +149,24 @@ public class ModelReader implements Closeable {
         float[] result = new float[numValues];
         for (int i = 0; i < numValues; i++) {
             result[i] = byteBuffer.getFloat();
+        }
+
+        return result;
+    }
+
+    public double[] readDoubleArrayBE(int numValues) throws IOException {
+        int numBytesRead = fillBuffer(numValues * 8);
+        if (numBytesRead < numValues * 8) {
+            throw new EOFException(
+                    String.format("Cannot read double array (shortage): expected = %d, actual = %d",
+                            numValues * 8, numBytesRead));
+        }
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(buffer).order(ByteOrder.BIG_ENDIAN);
+
+        double[] result = new double[numValues];
+        for (int i = 0; i < numValues; i++) {
+            result[i] = byteBuffer.getDouble();
         }
 
         return result;
@@ -155,6 +195,79 @@ public class ModelReader implements Closeable {
         }
 
         return new String(buffer, 0, numBytes, Charset.forName("UTF-8"));
+    }
+
+    public String readUTF() throws IOException {
+        int utflen = readByteAsInt();
+        utflen = (short)((utflen << 8) | readByteAsInt());
+        return readUTF(utflen);
+    }
+
+    public String readUTF(int utflen) throws IOException {
+        int numBytesRead = fillBuffer(utflen);
+        if (numBytesRead < utflen) {
+            throw new EOFException(
+                    String.format("Cannot read UTF string bytes: expected = %d, actual = %d",
+                            utflen, numBytesRead));
+        }
+
+        char[] chararr = new char[utflen];
+
+        int c, char2, char3;
+        int count = 0;
+        int chararr_count=0;
+
+        while (count < utflen) {
+            c = (int) buffer[count] & 0xff;
+            if (c > 127) break;
+            count++;
+            chararr[chararr_count++]=(char)c;
+        }
+
+        while (count < utflen) {
+            c = (int) buffer[count] & 0xff;
+            switch (c >> 4) {
+                case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+                    /* 0xxxxxxx*/
+                    count++;
+                    chararr[chararr_count++]=(char)c;
+                    break;
+                case 12: case 13:
+                    /* 110x xxxx   10xx xxxx*/
+                    count += 2;
+                    if (count > utflen)
+                        throw new UTFDataFormatException(
+                                "malformed input: partial character at end");
+                    char2 = (int) buffer[count-1];
+                    if ((char2 & 0xC0) != 0x80)
+                        throw new UTFDataFormatException(
+                                "malformed input around byte " + count);
+                    chararr[chararr_count++]=(char)(((c & 0x1F) << 6) |
+                            (char2 & 0x3F));
+                    break;
+                case 14:
+                    /* 1110 xxxx  10xx xxxx  10xx xxxx */
+                    count += 3;
+                    if (count > utflen)
+                        throw new UTFDataFormatException(
+                                "malformed input: partial character at end");
+                    char2 = (int) buffer[count-2];
+                    char3 = (int) buffer[count-1];
+                    if (((char2 & 0xC0) != 0x80) || ((char3 & 0xC0) != 0x80))
+                        throw new UTFDataFormatException(
+                                "malformed input around byte " + (count-1));
+                    chararr[chararr_count++]=(char)(((c     & 0x0F) << 12) |
+                            ((char2 & 0x3F) << 6)  |
+                            ((char3 & 0x3F) << 0));
+                    break;
+                default:
+                    /* 10xx xxxx,  1111 xxxx */
+                    throw new UTFDataFormatException(
+                            "malformed input around byte " + count);
+            }
+        }
+        // The number of chars produced may be less than utflen
+        return new String(chararr, 0, chararr_count);
     }
 
     @Override
